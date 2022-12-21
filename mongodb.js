b3c3b9520ff43a7e65d6f1e95c6a4e7a71a86a74
Anurag Dhote <anuragdhote392@gmail.com>
2022-12-21 16:08:11 +0530

    2nd Commit : ObjectIds

--- mongodb.js
+++ mongodb.js
@@ -1,11 +1,17 @@
 // CRUD create read update delete
 
-import mongodb from 'mongodb';
-const MongoClient = mongodb.MongoClient;
-// import { MongoClient } from "mongodb";
+// import mongodb from 'mongodb';
+// const MongoClient = mongodb.MongoClient;
+// const ObjectID = mongodb.ObjectId;
+import { MongoClient, ObjectId} from "mongodb";
 
 const connectionURL = 'mongodb://127.0.0.1:27017';
 const databaseName = 'task-manager';
+
+const id = new ObjectId();
+console.log(id.id.length)
+console.log(id.toHexString().length);
+// console.log(id.getTimestamp())
 
 MongoClient.connect(connectionURL, { useNewUrlParser : true }, (error, client) => {
     if (error) {
@@ -15,16 +21,62 @@ MongoClient.connect(connectionURL, { useNewUrlParser : true }, (error, client) =
     // console.log('Connected Successfully');
     const db = client.db(databaseName);
     
-    db.collection('users').insertOne({
-        name : 'Anurag',
-        age : 27
-    }, async (error, result) => {
-        if (error) {
-            return console.log('Unable to insert user');
-        }
-        console.log('Inserted Documents : ');
+    // db.collection('users').insertOne({
+    //     _id: id,
+    //     name : 'Anurag',
+    //     age : 27
+    // }, async (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert user');
+    //     }
+    //     console.log('Inserted Documents : ');
 
-        const insertedDocs = await db.collection('users').find({"_id": result.insertedId}).toArray();
-        console.log(insertedDocs);
-    })
-})
+    //     const insertedDocs = await db.collection('users').find({"_id": result.insertedId}).toArray();
+    //     console.log(insertedDocs);
+
+    //     client.close();
+    // })
+
+    // db.collection('users').insertMany([
+    //     {
+    //         name : 'Jen',
+    //         age : 27
+    //     },
+    //     {
+    //         name : 'Gunther',
+    //         age : 28
+    //     }
+    // ], async (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert user');
+    //     }
+
+    //     console.log('Inserted Docs successfully.');
+
+    //     client.close()
+    // });
+
+
+    // db.collection('tasks').insertMany([
+    //     {
+    //         description : 'Chant Hare Krishna Mahamantra',
+    //         completed: false
+    //     },
+    //     {
+    //         description : 'Complete GCCP campaign',
+    //         completed: false
+    //     },
+    //     {
+    //         description : 'Restart Node Project',
+    //         completed : true
+    //     }
+    // ], async (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert user');
+    //     }
+
+    //     console.log(result)
+
+    //     client.close()
+    // })
+});
