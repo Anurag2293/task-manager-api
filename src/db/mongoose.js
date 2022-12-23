bb417db6aefcc7a9b449a955c7ed9c7f0c934616
Anurag Dhote <anuragdhote392@gmail.com>
2022-12-23 18:03:00 +0530

    9th Commit : Finish Create & Read Endpoint

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -5,26 +5,3 @@ export default function connectDB () {
     mongoose.set('strictQuery', false);
     mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api');
 }
-
-const Task = mongoose.model('Task', {
-    description : {
-        type : String,
-        required : true,
-        trim : true
-    },
-    completed : {
-        type : Boolean,
-        default : false
-    }
-})
-
-const task = new Task({
-    description : 'Chant Hare Krishna Mahamantra',
-    completed : true
-})
-
-// task.save().then(() => {
-//     console.log(task);
-// }).catch((error) => {
-//     console.log(error)
-// })
