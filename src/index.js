3e44e11e41130f84f5c44af1acf92057725a4ce7
Anurag Dhote <anuragdhote392@gmail.com>
2022-12-24 11:31:35 +0530

    14th Commit : Hashing Tutorial

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import bcrypt from 'bcryptjs';
 import connectDB from './db/mongoose.js';
 import userRouter from './routers/user.js';
 import taskRouter from './routers/task.js';
@@ -15,3 +16,16 @@ app.use(taskRouter);
 app.listen(port, () => {
     console.log('Process is running on port', port);
 });
+
+const myFunction = async () => {
+    const password = 'Red12345@';
+    const hashedPassword = await bcrypt.hash(password, 8);
+
+    console.log(password);
+    console.log(hashedPassword);
+
+    const isMatch = await bcrypt.compare('Red12345@', hashedPassword);
+    console.log(isMatch);
+}
+
+myFunction();
